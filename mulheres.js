b1ab5a62e956f949ea315a69656a17a074a94610
MarcilaneSouza <marcilane29@gmail.com>
2023-04-23 23:46:22 -0300

    add post e o patch

--- mulheres.js
+++ mulheres.js
@@ -1,34 +1,86 @@
-const express = require("express")
-const router = express(express.Router)
+const express = require("express")  //Aqui estou iniciando o express
+const router = express.Router() //aqui estou configurando a primeira parte da rota
+const { v4: uuidv4 } = require('uuid')
 
-const app = express()
-const porta = 3333
 
+const app = express() //aqui estou iniciando  app
+app.use(express.json())
+const porta = 3333 // aqui estou criando a porta
+
+// aqui estou criando lista inicial de mulheres
 const mulheres = [
     {
+        id: '1',
         nome: 'Marcilane Souza',
         Imagem: 'https://github.com/MarcilaneSouza.png',
         minibio: 'Desenvolvedora Backend'
     },
     {
+        id: '2',
         nome: 'Marcilane Souza',
         Imagem: 'https://github.com/MarcilaneSouza.png',
         minibio: 'Desenvolvedora Backend'
     },
     {
+        id: '3',
         nome: 'Marcilane Souza',
         Imagem: 'https://github.com/MarcilaneSouza.png',
         minibio: 'Desenvolvedora Backend'
     }
 ]
 
+// GET
 function mostraMulheres(request, response) {
     response.json(mulheres)
 }
 
+//POST
+function criaMulher(request, response) {
+    const novaMulher = {
+        id: uuidv4(),
+        nome: request.body.nome,
+        imagem: request.body.imagem,
+        minibio: request.body.minibio
+    }
+    mulheres.push(novaMulher)
+
+    response.json(mulheres)
+}
+
+// PATCH
+function corrigeMulher (request, response) {
+    function encontraMulher(mulher) {
+        if (mulher.id === request.params.id) {
+            return mulher
+        }
+
+    }
+    
+    const mulherEncontrada = mulheres.find(encontraMulher)
+
+    if (request.body.nome) {
+        mulherEncontrada.nome = request.body.nome
+    }
+
+    if (request.body.minibio) {
+        mulherEncontrada.minibio = request.body.minibio
+    } 
+
+    if(request.body.imagem) {
+        mulherEncontrada = request.body.imagem
+    }
+
+    response.json(mulheres)
+
+}
+
+app.use(router.get('/mulheres', mostraMulheres)) //configurei rota GET /mulheres
+app.use(router.post('/mulheres', criaMulher)) //configurei rota POST /mulheres
+app.use(router.patch('/mulheres/:id', corrigeMulher)) //configurei rota PATCH /mulheres/:id
+
+// PORTA
 function mostraPorta() {
     console.log("Servidor criado e rodando na porta", porta)
 }
 
-app.use(router.get('/mulheres', mostraMulheres))
-app.listen(porta, mostraPorta) 
+app.listen(porta, mostraPorta)  // servidor ouvindo a porta
